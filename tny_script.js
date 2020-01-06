7e05de58ba6e48e303f9513078577a81f53b8a37
Samuel <sharris163@west-mec.org>
2020-01-06 15:30:34 -0700

    day 1

---
+++ tny_script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Samuel Harris
+   Date:   1/6/20
+
+*/
+
+document.getElementsById("dateNow");
+
+document.getSelection.name("Jeff");
