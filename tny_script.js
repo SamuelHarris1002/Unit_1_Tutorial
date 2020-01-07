99fc74778d2322bd67446f8ecad7487dba075b02
Samuel <sharris163@west-mec.org>
2020-01-07 15:07:43 -0700

    day 2

--- tny_script.js
+++ tny_script.js
@@ -11,6 +11,18 @@
 
 */
 
-document.getElementsById("dateNow");
+//Display the current date and time
+document.getElementById("dateNow").innerHTML="m/d/y <br/> h:m:s"; 
 
-document.getSelection.name("Jeff");
+//Display the time left until New Year's Eve
+document.getElementById("days").textContent = "DD";
+document.getElementById("hrs").textContent = "HH";
+document.getElementById("mins").textContent = "MM";
+document.getElementById("secs").textContent = "SS";
+
+var date = new Date("January 7, 2020 13:54:23");
+
+var dateStr = date.toDateString(); // dateStr == 1/7/2020
+var timeStr = date.toLocaleDateString(); // timeStr == 1:54:23 pm
+
+document.getElementById("dateNow").innerHTML = dateStr + "<br/>" + timeStr;
